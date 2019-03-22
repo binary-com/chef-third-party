31700fa6120334228ed4e3de1d5a0987717d2d22
laddanki <aditya@binary.com>
2019-03-22 10:38:50 +0800

    upgrade

--- cookbooks/chef_client_updater/attributes/default.rb
+++ cookbooks/chef_client_updater/attributes/default.rb
@@ -23,7 +23,7 @@ default['chef_client_updater']['channel'] = 'stable'
 default['chef_client_updater']['prevent_downgrade'] = false
 
 # the version to install (ex: '12.12.13') or 'latest'
-default['chef_client_updater']['version'] = '13.0.118'
+default['chef_client_updater']['version'] = 'latest'
 
 # kill the client post install or exec the client post install for non-service based installs
 default['chef_client_updater']['post_install_action'] = Chef::Config[:no_fork] ? 'exec' : 'kill'
