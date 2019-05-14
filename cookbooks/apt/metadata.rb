1911a2ff3e4f0c8e44d1c15d5282531946c1f294
laddanki <aditya@binary.com>
2019-05-14 14:40:48 +0800

    apt upgraded

--- cookbooks/apt/metadata.rb
+++ cookbooks/apt/metadata.rb
@@ -1,10 +1,10 @@
 name 'apt'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
-description 'Configures apt and apt services. Ships resources for managing apt repositories'
+license 'Apache-2.0'
+description 'Configures apt and apt caching.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '4.0.2'
+version '7.1.1'
 
 recipe 'apt::default', 'Runs apt-get update during compile phase and sets up preseed directories'
 recipe 'apt::cacher-ng', 'Set up an apt-cacher-ng caching proxy'
@@ -16,7 +16,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/apt'
 issues_url 'https://github.com/chef-cookbooks/apt/issues'
-
-chef_version '>= 12.1' if respond_to?(:chef_version)\
-
-depends 'compat_resource', '>= 12.10'
+chef_version '>= 13.3' if respond_to?(:chef_version)
