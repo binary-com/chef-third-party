81ced31f16f35ad22018845cac9a2350922df4aa
Aditya-Binary <aditya-binary@binary.com>
2019-11-12 13:42:17 +0800

    removed chef_handler and sysctl

--- cookbooks/chef_handler/recipes/default.rb
+++
@@ -1,1 +0,0 @@
-Chef::Log.warn('chef_handler::default recipe has been deprecated. Use the chef_handler resource instead to define handlers from your own wrapper cookbook.')
--- cookbooks/sysctl/spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'chefspec'
-require 'chefspec/berkshelf'
-
-RSpec.configure do |config|
-  config.formatter = :documentation
-  config.color = true
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
-end
