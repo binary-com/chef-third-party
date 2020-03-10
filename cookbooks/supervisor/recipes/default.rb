6ce16b76658de5fa1d4b32a6942a37a550018716
Afshin Paydar <afshin.paydar@binary.com>
2020-03-10 09:14:30 +0000

    load variables at runtime for supervisord.conf

--- cookbooks/supervisor/recipes/default.rb
+++ cookbooks/supervisor/recipes/default.rb
@@ -41,13 +41,17 @@ template node['supervisor']['conffile'] do
   owner 'root'
   group 'root'
   mode '644'
-  variables(inet_port: node['supervisor']['inet_port'],
-            inet_username: node['supervisor']['inet_username'],
-            inet_password: node['supervisor']['inet_password'],
-            supervisord_minfds: node['supervisor']['minfds'],
-            supervisord_minprocs: node['supervisor']['minprocs'],
-            supervisor_version: node['supervisor']['version'],
-            socket_file: node['supervisor']['socket_file'])
+  variables((lazy do
+               {
+                inet_port: node['supervisor']['inet_port'],
+                inet_username: node['supervisor']['inet_username'],
+                inet_password: node['supervisor']['inet_password'],
+                supervisord_minfds: node['supervisor']['minfds'],
+                supervisord_minprocs: node['supervisor']['minprocs'],
+                supervisor_version: node['supervisor']['version'],
+                socket_file: node['supervisor']['socket_file'],
+               }
+            end)
 end
 
 directory node['supervisor']['log_dir'] do
