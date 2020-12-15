e79d54bdd2ab0852114d7d7b4bf9c9548e8a2b00
mostafa-fs <mostafa.zare@firstsource.io>
2020-12-15 13:46:34 +0330

    docker dependenies

---
+++ cookbooks/docker_test/recipes/installation_package.rb
@@ -0,0 +1,4 @@
+docker_installation_package 'default' do
+  version '19.03.8'
+  action :create
+end
---
+++ cookbooks/docker_test/recipes/installation_tarball.rb
@@ -0,0 +1,4 @@
+docker_installation_tarball 'default' do
+  version '19.03.5'
+  action :create
+end
