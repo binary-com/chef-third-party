9da32c90aa5b11d963d73153577a86d153a2d990
khushal-deriv <khushal@deriv.com>
2021-10-12 15:47:16 +0530

    selinux, resolver, rabbitmq cookbook update

---
+++ cookbooks/rabbitmq/test/integration/lwrps/default_spec.rb
@@ -0,0 +1,14 @@
+# The Inspec reference, with examples and extensive documentation, can be
+# found at https://inspec.io/docs/reference/resources/
+
+describe command('curl -u guest:guest -H "Accept: application/json" -X GET "http://localhost:15672/api/overview"') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('rabbitmqctl list_policies -s') do
+  its(:stdout) { should match(%r{{"max-length":1000}}) }
+end
+
+describe command('rabbitmqctl list_parameters -s -p sensu') do
+  its(:stdout) { should match(%r{federation-upstream\s+sensu-dc-1\s+{"uri":"amqp:\/\/dc-cluster-node"}}) }
+end
