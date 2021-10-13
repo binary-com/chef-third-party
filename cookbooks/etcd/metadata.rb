89eadaae65d2cd69aae5823b6b84bd19bc886adc
khushal-deriv <khushal@deriv.com>
2021-10-13 10:36:00 +0530

    git, etcd, erlang, dpkg_autostart update

--- cookbooks/etcd/metadata.rb
+++ cookbooks/etcd/metadata.rb
@@ -3,10 +3,10 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures etcd'
-version           '6.3.0'
+version           '7.0.2'
 source_url        'https://github.com/sous-chefs/etcd'
 issues_url        'https://github.com/sous-chefs/etcd/issues'
-chef_version      '>= 12.15'
+chef_version      '>= 15.3'
 
 depends 'docker'
 
