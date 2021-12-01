bdc2371ff7f69f301b23bb5feb0c27a1e7040760
khushal-deriv <khushal@deriv.com>
2021-12-01 13:50:23 +0800

    edit in helpers_json.rb to support our code

--- cookbooks/docker/libraries/helpers_json.rb
+++ cookbooks/docker/libraries/helpers_json.rb
@@ -1,11 +1,11 @@
 module DockerCookbook
   module DockerHelpers
     module Json
-      def generate_json(dangling, prune_until, with_label, without_label)
+      def generate_json(dangling, prune_until)
         opts = { dangling: { "#{dangling}": true } }
         opts['until'] = { "#{prune_until}": true } unless prune_until == nil?
-        opts['label'] = { "#{with_label}": true } unless with_label == nil?
-        opts['label!'] = { "#{without_label}": true } unless without_label == nil?
+#       opts['label'] = { "#{with_label}": true } unless with_label == nil?
+#       opts['label!'] = { "#{without_label}": true } unless without_label == nil?
         'filters=' + URI.encode_www_form_component(opts.to_json)
       end
     end
