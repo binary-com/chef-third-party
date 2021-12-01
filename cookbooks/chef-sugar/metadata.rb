0dd32fd6dfa111c3883021a326a89c5b7c29daa2
khushal-deriv <khushal@deriv.com>
2021-12-01 16:44:28 +0800

    adding latest chef-sugar code

--- cookbooks/chef-sugar/metadata.rb
+++ cookbooks/chef-sugar/metadata.rb
@@ -2,8 +2,7 @@ name             'chef-sugar'
 maintainer       'Chef Software, Inc'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
-description      'Installs chef-sugar. Please see the chef-sugar ' \
-                 'Ruby gem for more information.'
+description      'Installs chef-sugar. Please see the chef-sugar Ruby gem for more information.'
 long_description <<-EOH
 Chef Sugar is a Gem & Chef Recipe that includes series of helpful syntactic
 sugars on top of the Chef core and other resources to make a cleaner, more lean
@@ -14,11 +13,10 @@ For the most up-to-date information and documentation, please visit the [Chef
 Sugar project page on GitHub](https://github.com/chef/chef-sugar).
 EOH
 
-require          File.expand_path('../lib/chef/sugar/version', __FILE__)
-version          Chef::Sugar::VERSION
+version '5.1.12'
 
 supports     'any'
 issues_url   'https://github.com/chef/chef-sugar/issues'
 source_url   'https://github.com/chef/chef-sugar'
 chef_version '>= 13.0'
-gem          'chef-sugar-ng'
+gem          'chef-sugar'
