c547d041e4e9edf65c2a89313fb4371983ba431a
Khushal-deriv <khushal@deriv.com>
2022-03-02 14:31:00 +0800

    bind update

--- cookbooks/bind/resources/key.rb
+++ cookbooks/bind/resources/key.rb
@@ -1,9 +1,14 @@
-# frozen_string_literal: true
+unified_mode true
+
 KeyOptions = Struct.new(:name, :algorithm, :secret)
 
-property :algorithm, String
-property :bind_config, String, default: 'default'
-property :secret, String
+property :algorithm, String,
+          description: 'The algorithm that the secret key was generated from'
+property :bind_config, String,
+          default: 'default',
+          description: 'Name of the bind_config resource to notify actions on'
+property :secret, String,
+          description: 'The secret key'
 
 action :create do
   config_template.variables[:keys] << KeyOptions.new(
