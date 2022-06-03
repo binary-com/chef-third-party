1340f8ffbcbda54da7dcd5d5ba8e7a3b24a0650b
Khushal-deriv <81961842+Khushal-deriv@users.noreply.github.com>
2022-06-03 13:40:19 +0800

    added cron latest cookbook (#89)
    * added cron latest ookbook
    
    * adding recipes folder and default.rb for cron
    
    * cron changes
    
    * cron error fix
    
    * cron fix

--- cookbooks/cron/resources/d.rb
+++ cookbooks/cron/resources/d.rb
@@ -17,76 +17,87 @@
 # limitations under the License.
 #
 
-unified_mode true
+unified_mode true if respond_to?(:unified_mode)
 
-chef_version_for_provides '< 14.4' if respond_to?(:chef_version_for_provides)
-resource_name :cron_d
-provides :cron_d
+property :cron_name, String,
+          name_property: true
 
-require 'shellwords'
+property :cookbook, String,
+          default: 'cron'
 
-property :cron_name, String, name_property: true
-property :cookbook, String, default: 'cron'
-property :predefined_value, String, equal_to: %w( @reboot @yearly @annually @monthly @weekly @daily @midnight @hourly )
-property :minute, [Integer, String], default: '*', callbacks: {
-  'should be a valid minute spec' => lambda do |spec|
-    ::Cron::Helpers.validate_numeric(spec, 0, 59)
-  end,
-}
-property :hour, [Integer, String], default: '*', callbacks: {
-  'should be a valid hour spec' => ->(spec) { ::Cron::Helpers.validate_numeric(spec, 0, 23) },
-}
-property :day, [Integer, String], default: '*', callbacks: {
-  'should be a valid day spec' => ->(spec) { ::Cron::Helpers.validate_numeric(spec, 1, 31) },
-}
-property :month, [Integer, String], default: '*', callbacks: {
-  'should be a valid month spec' => ->(spec) { ::Cron::Helpers.validate_month(spec) },
-}
-property :weekday, [Integer, String], default: '*', callbacks: {
-  'should be a valid weekday spec' => ->(spec) { ::Cron::Helpers.validate_dow(spec) },
-}
+property :predefined_value, String,
+          equal_to: %w( @reboot @yearly @annually @monthly @weekly @daily @midnight @hourly )
 
-property :command, String, required: true
-property :user, String, default: 'root'
-property :mailto, [String, NilClass]
-property :path, [String, NilClass]
-property :home, [String, NilClass]
-property :shell, [String, NilClass]
-property :comment, [String, NilClass]
-property :environment, Hash, default: {}
-property :mode, [String, Integer], default: '0600'
-property :random_delay, [Integer, NilClass]
+property :minute, [Integer, String],
+          default: '*',
+          callbacks: {
+                       'should be a valid minute spec' => ->(spec) { ::Cron::Cookbook::CronHelpers.validate_numeric(spec, 0, 59) },
+                     }
 
-def after_created
-  raise 'The cron_d resource requires Linux as it needs support for the cron.d directory functionality.' unless node['os'] == 'linux'
-end
+property :hour, [Integer, String],
+          default: '*',
+          callbacks: {
+                       'should be a valid hour spec' => ->(spec) { ::Cron::Cookbook::CronHelpers.validate_numeric(spec, 0, 23) },
+                     }
 
-action :create do
-  create_template(:create)
-end
+property :day, [Integer, String],
+          default: '*',
+          callbacks: {
+                       'should be a valid day spec' => ->(spec) { ::Cron::Cookbook::CronHelpers.validate_numeric(spec, 1, 31) },
+                     }
 
-action :create_if_missing do
-  create_template(:create_if_missing)
-end
+property :month, [Integer, String],
+          default: '*',
+          callbacks: {
+                       'should be a valid month spec' => ->(spec) { ::Cron::Cookbook::CronHelpers.validate_month(spec) },
+                     }
 
-action :delete do
-  # cleanup the legacy named job if it exists
-  file 'legacy named cron.d file' do
-    path "/etc/cron.d/#{new_resource.cron_name}"
-    action :delete
-  end
+property :weekday, [Integer, String],
+          default: '*',
+          callbacks: {
+                      'should be a valid weekday spec' => ->(spec) { ::Cron::Cookbook::CronHelpers.validate_dow(spec) },
+                     }
 
-  file "/etc/cron.d/#{sanitized_name}" do
-    action :delete
-  end
-end
+property :command, String
+
+property :user, String,
+          default: 'root'
+
+property :mailto, String
+
+property :path, String
+
+property :home, String
+
+property :shell, String
+
+property :comment, String
+
+property :environment, Hash,
+          default: {}
+
+property :mode, [String, Integer],
+          default: '0600'
+
+property :random_delay, Integer
 
 action_class do
   def sanitized_name
     new_resource.cron_name.tr('.', '-')
   end
 
+  def required_properties_set?
+    raise Chef::Exceptions::ValidationFailed, 'command is a required property' unless action.eql?(:delete) || !new_resource.command.nil?
+  end
+
+  def platform_check
+    raise 'The cron_d resource requires Linux as it needs support for the cron.d directory functionality.' unless node['os'] == 'linux'
+  end
+
   def create_template(create_action)
+    platform_check
+    required_properties_set?
+
     # cleanup the legacy named job if it exists
     file "#{new_resource.cron_name} legacy named cron.d file" do
       path "/etc/cron.d/#{new_resource.cron_name}"
@@ -118,5 +129,21 @@ action_class do
       )
       action create_action
     end
+  end
+end
+
+%i(create create_if_missing).each do |action_type|
+  send(:action, action_type) { create_template(action) }
+end
+
+action :delete do
+  # cleanup the legacy named job if it exists
+  file 'legacy named cron.d file' do
+    path "/etc/cron.d/#{new_resource.cron_name}"
+    action :delete
+  end
+
+  file "/etc/cron.d/#{sanitized_name}" do
+    action :delete
   end
 end
--- cookbooks/cron/test/integration/default/inspec/cron_spec.rb
+++ cookbooks/cron/test/integration/default/inspec/cron_spec.rb
@@ -5,14 +5,10 @@ puts "The OS release is #{os[:release]}"
 puts "The OS name is #{os[:name]}"
 
 if %w(debian ubuntu fedora redhat).include?(os[:family])
-  if %w(redhat fedora).include?(os[:family])
+  if %w(redhat fedora amazon).include?(os[:family])
     service_name = 'crond'
     package_name = 'cronie'
 
-    # RHEL 7+ check
-    unless os[:name] == 'amazon' && os[:release].to_i >= 6
-      package_name = 'vixie-cron'
-    end
   elsif %w(debian ubuntu).include?(os[:family])
     service_name = 'cron'
     package_name = 'cron'
@@ -30,10 +26,10 @@ end
 
 # make sure the :create_if_missing didn't overwrite the :create
 describe file('/etc/cron.d/test-weekday-usage-report') do
-  its(:content) { should match(/\/srv\/app\/scripts\/generate_report/) }
+  its(:content) { should match(%r{/srv/app/scripts/generate_report}) }
 end
 describe file('/etc/cron.d/nil_value_check') do
-  its(:content) { should match(/\* \* \* \* \* appuser \/bin\/true/) }
+  its(:content) { should match(%r{\* \* \* \* \* appuser /bin/true}) }
 end
 describe file('/etc/cron.d/delete_cron') do
   it { should_not exist }
